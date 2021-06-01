a28616bbe671c914b5884b600a5a830ec83188dd
Javin <47981364+javin7@users.noreply.github.com>
2021-06-01 08:13:03 -0400

    Create CCC '15 J1 - Special Day.java

---
+++ CCC '15 J1 - Special Day.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int month = in.nextInt();
+        int day = in.nextInt();
+
+        if (month < 2) {
+            System.out.println("Before");
+        } else if (month > 2) {
+            System.out.println("After");
+        } else {
+            if (day < 18) {
+                System.out.println("Before");
+            } else if (day > 18) {
+                System.out.println("After");
+            } else {
+                System.out.println("Special");
+            }
+        }
+    }
+}
